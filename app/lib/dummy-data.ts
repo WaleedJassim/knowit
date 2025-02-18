03b4786df005bd57994bacbff9971c072d27fffb
Jassim <waleed.jassim@zartek.in>
2025-02-18 18:11:28 +0530

    Adds course page

--- app/lib/dummy-data.ts
+++ app/lib/dummy-data.ts
@@ -98,4 +98,45 @@ export const courses: Course[] = [
 			{ src: 'https://github.com/shadcn.png', alt: 'User 8', fallback: 'U8' },
 		],
 	},
+	{
+		id: 3,
+		imageUrl:
+			'https://s3-alpha-sig.figma.com/img/ceea/5d61/0d370b63bf2e6a6d60edbea9cc75f1c1?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=sb6DhzSnq57bJLW1PYwJJdxDJatPlraww9DBULtu11-VndDgFJXFfP8AxiKxMRUWr~P1yPLFpD29J0vK~SJw9J~PH42pqTkLf3zHCijFrPwUHTaoxvgR~ujdLpN8~LK7IAh5WN1jjldrtzpGyzTYBYlTmOO9Lp5gV~hQ6tdhdhltPjNRsMLWS3jA06~EdfeJYGammufX5T3048KUx26xwB0di1fB9EKL64u68ZeuvMm4ofpPEXzDPSHn5rTwUepoWUWC6h1UmhaPev~W1BpYpdEqsEYMuWUD0JPA3eAmPd5nafrpUZMQA8eqS2c16qHSXuHPhmP2O8dC9EPsW~592g__',
+		courseName: 'Piping And pipeline Engineering',
+		description:
+			'Knowit Education offers the Piping Design course  The industry offers a diverse range of jobs in pipelines,',
+		rating: 4.0,
+		discountedPrice: 999,
+		originalPrice: 1499,
+		students: [
+			{
+				src: 'https://s3-alpha-sig.figma.com/img/2386/8fe3/2fe621cb5273ca12b6688301cd72e7c7?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KI500cF4--yWWFnb4lDbhXAZgBvERPTBtG7~bk0Eblg-fZ45m5S7D3lIs2OsW6qCKfWCm5kOQ~oDKYAteruWXLGEBzXXRENUZ5TruZXCeWSb4Exrlt8Nt1buv5RjA2tEmV91fWq5f~UZEPHWXr~DnuiVRnT4XysgmiTkbAtVswGFs63wnG-qLtMQag9QdyMr7VOh5RVkDnPs9gDFIoZqdCSnEB5N9y6tKhiPNRnzpwSMaLQtiwDEanTTjOkR5K4hwTlV3K6LcyiHDIp~fetfgqHDHoQfbOACFFYZr-c8jLIDdyMHNMx0Y-7QPuH6WiOKwO84Ml28M4nGz~Nh8tlRPw__',
+				alt: 'User 1',
+				fallback: 'U1',
+			},
+			{
+				src: 'https://s3-alpha-sig.figma.com/img/bc22/9062/6c7234ac65bb2a13712b1a387fb79525?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=sAkgkP4ylKoJqGNpsT-NKQ-cDpMEraLo~NeeuTQQE-YTawkTJ-hLBQK926gEzPL0UwZw71OzS-QnVxRTk1x5qqTUFW7qt8OXYW4~fJrpTXRnSII7lQnDFM9LgoPGXekRCFhuB50GVX8TPXMMLABCeU4Gkl23hkMqY0QsFtqlX6EZB9oEv2Mx4ZcfRwsrFGWda39PhJ4nPfu5IGIn05hFr2IbWXaqAnwqidE-GFbb59zGMMlJJahiURMmPPorQi7aQpTXKIfKTI6w8n0dKhToNCis81cz8HUuSStdT~Ly5I~41y63D8C7I~JQ~j7KA1behMcS9O95qgSsvLRcAfbC9A__',
+				alt: 'User 2',
+				fallback: 'U2',
+			},
+			{
+				src: 'https://s3-alpha-sig.figma.com/img/bc22/9062/6c7234ac65bb2a13712b1a387fb79525?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=sAkgkP4ylKoJqGNpsT-NKQ-cDpMEraLo~NeeuTQQE-YTawkTJ-hLBQK926gEzPL0UwZw71OzS-QnVxRTk1x5qqTUFW7qt8OXYW4~fJrpTXRnSII7lQnDFM9LgoPGXekRCFhuB50GVX8TPXMMLABCeU4Gkl23hkMqY0QsFtqlX6EZB9oEv2Mx4ZcfRwsrFGWda39PhJ4nPfu5IGIn05hFr2IbWXaqAnwqidE-GFbb59zGMMlJJahiURMmPPorQi7aQpTXKIfKTI6w8n0dKhToNCis81cz8HUuSStdT~Ly5I~41y63D8C7I~JQ~j7KA1behMcS9O95qgSsvLRcAfbC9A__',
+				alt: 'User 3',
+				fallback: 'U3',
+			},
+			{
+				src: 'https://s3-alpha-sig.figma.com/img/f8fd/c7b5/52f480f3420d4c1f0a09af5a01c4723f?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=e4FTWZijeazMa2THlzqwjsylwzJgzeY30ZO5n0Eze8tzxLKk-MDTt6As9me051Fg0hEQTZ9s0u-4ZfV9nKwqOG8GVYfE4dycOqko-~bpl6LOkeDP7TzghS4GhOoUFsxaTMGPrTRptIgAyejjQPfV84E6ocJyQJKXiCvE9EYhW4LQot7Q7HQJA0t7nJGhkNAeXVplnsRyzflUw9fc18wSao1QjZTZCd2n2MFwdHXK5kjq2kwyVS6yGQO2ubkKzJrFvuhpTCqfBDgFYNa8B-Vc36eWc-xvSe45MfuK5OFFR7H5tVY9yc4puQgteP3nmEM1cUg-2v92xEaUAVv7eyiGTQ__',
+				alt: 'User 4',
+				fallback: 'U4',
+			},
+			{
+				src: 'https://s3-alpha-sig.figma.com/img/f706/92fa/93ad65ab736b95a1530fd07f86463b22?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=K8Mq9MQx4g1Ej2RQCXXU1mbluQzGWZJYw4MkkbpN2uxoNGz5iXBcLq0t48xvN8f-8~UjgbClh62~IVu3sADvCzirWlBnpuLAzLQJujiyY58k1OqXq-bWt0EztBtlI1dBTf6hAW9y0pv1J4e3ohktafNa8VA6wTaAj0vTPUSycobXQM145HXVAQnYnoE2FzZNy6G~z8koKuEA2Hp-wEPCednnukVKsAKyVd5p-7TpOhnAir35J5Zp9TmKw~wMBfoA1TKyCWWUFU4kXIbnEyHqBYNZ-bKWmwxUFifGjqqw7hsCULMr296Rhp9j1vSNYiI3gUHGbCib7Pr3ofGV-S7X5w__',
+				alt: 'User 5',
+				fallback: 'U5',
+			},
+			{ src: 'https://github.com/shadcn.png', alt: 'User 6', fallback: 'U6' },
+			{ src: 'https://github.com/shadcn.png', alt: 'User 7', fallback: 'U7' },
+			{ src: 'https://github.com/shadcn.png', alt: 'User 8', fallback: 'U8' },
+		],
+	},
 ];
--- app/pages/courses/index.tsx
+++ app/pages/courses/index.tsx
@@ -1,5 +1,48 @@
+import { Filter } from 'lucide-react';
+import CourseCard from '~/components/shared/course-card';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { courses } from '~/lib/dummy-data';
+
+const filters = ['Mechanical', 'Healthcare', 'NDT', 'Infrastructure'];
+
 const CoursesPage = () => {
-	return <div></div>;
+	return (
+		<div className="p-6 flex flex-col gap-8">
+			<div className="flex flex-col gap-2">
+				<p className="text-lg font-medium">
+					Enjoy exploring from our learining world
+				</p>
+				<p className="text-4xl font-bold text-(--color-primary)">
+					Start learning with us
+				</p>
+			</div>
+			<div className="bg-background flex flex-col gap-8">
+				<div className="flex items-center gap-4 justify-between">
+					<div className="flex items-center gap-4">
+						<Input
+							placeholder="Search your Course"
+							className="sm:w-md rounded-2xl"
+						/>
+						{filters.map((item, index) => (
+							<Button key={index} className="bg-white border-white">
+								{item}
+							</Button>
+						))}
+					</div>
+					<Button>
+						<Filter />
+						Filters
+					</Button>
+				</div>
+				<div className="flex flex-wrap gap-9">
+					{courses.map((course) => (
+						<CourseCard data={course} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CoursesPage;
